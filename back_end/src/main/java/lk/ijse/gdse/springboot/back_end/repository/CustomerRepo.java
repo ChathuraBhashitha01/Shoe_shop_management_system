4813f9f9adc269b59d3fa52bd28827968c7e4f7b
MSI <chathurabhashitha01@gmail.com>
2024-04-30 00:58:42 +0530

    customer repo creatio

---
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/repository/CustomerRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.springboot.back_end.repository;
+
+import lk.ijse.gdse.springboot.back_end.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer,String> {
+
+}
