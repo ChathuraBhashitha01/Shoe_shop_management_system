fe42e58ec1dcbc6b61b33b5bf0999a74167ec48f
MSI <chathurabhashitha01@gmail.com>
2024-04-29 21:53:49 +0530

    customerdto creation

---
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse.springboot.back_end.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customers")
+@CrossOrigin
+public class CustomerController {
+
+}
