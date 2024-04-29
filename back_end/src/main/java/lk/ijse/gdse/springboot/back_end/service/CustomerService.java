2b2bf11d58d12a4d439057bc0bafb95bd1e4849f
MSI <chathurabhashitha01@gmail.com>
2024-04-30 00:56:31 +0530

    customerservice entity creation

---
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/service/CustomerService.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.springboot.back_end.service;
+
+import lk.ijse.gdse.springboot.back_end.dto.CustomerDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<CustomerDTO> getAllCustomers();
+    CustomerDTO getCustomerDetails(String id);
+    CustomerDTO saveCustomer(CustomerDTO customerDTO);
+    void updateCustomer(CustomerDTO customerDTO);
+    void deleteCustomer(String id);
+}
