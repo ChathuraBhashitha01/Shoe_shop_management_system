cdc8a7ca3b1f26b11d4d1251bbb57ed8f737368d
MSI <chathurabhashitha01@gmail.com>
2024-05-02 23:53:25 +0530

    inventory entity added

--- back_end/src/main/java/lk/ijse/gdse/springboot/back_end/entity/Inventory.java
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/entity/Inventory.java
@@ -1,4 +1,28 @@
 package lk.ijse.gdse.springboot.back_end.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "inventory")
 public class Inventory {
+
+    @Id
+    private String itemCode;
+    private String itemDesc;
+    private String itemPicture;
+    private String category;
+    private String size;
+    private String supplierCode;
+    private String supplierName;
+    private double unitPriceSale;
+    private double unitPriceBuy;
+    private double expectedProfit;
+    private double profitMargin;
+    private String status;
 }
