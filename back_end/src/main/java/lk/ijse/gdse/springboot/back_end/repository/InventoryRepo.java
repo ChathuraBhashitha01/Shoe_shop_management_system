1334b2e0a49f5ee15c7c958e0a0afcfa763ee8aa
MSI <chathurabhashitha01@gmail.com>
2024-05-03 11:57:47 +0530

    supplier repo creation

---
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/repository/InventoryRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse.springboot.back_end.repository;
+
+import lk.ijse.gdse.springboot.back_end.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepo extends JpaRepository<Inventory,String> {
+}
