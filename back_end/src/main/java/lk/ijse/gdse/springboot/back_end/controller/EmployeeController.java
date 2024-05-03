943b93f9cdce0fe8004a1cf5e1577f421b371317
MSI <chathurabhashitha01@gmail.com>
2024-05-03 20:52:06 +0530

    employee controller creation

--- back_end/src/main/java/lk/ijse/gdse/springboot/back_end/controller/EmployeeController.java
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/controller/EmployeeController.java
@@ -1,5 +1,44 @@
 package lk.ijse.gdse.springboot.back_end.controller;
 
-public class EmployeeController {
+import lk.ijse.gdse.springboot.back_end.dto.CustomerDTO;
+import lk.ijse.gdse.springboot.back_end.dto.EmployeeDTO;
+import lk.ijse.gdse.springboot.back_end.service.EmployeeService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EmployeeDTO> getAllEmployees(){
+        return employeeService.getAllEmployee();
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDTO getEmployeeDetails(@PathVariable("id") String id){
+        return employeeService.getEmployeeDetails(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveEmployee(@RequestBody EmployeeDTO employeeDTO){
+        employeeService.saveEmployee(employeeDTO);
+    }
+
+    @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateEmployee(@RequestBody EmployeeDTO employeeDTO){
+        employeeService.updateEmployee(employeeDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteEmployee(@PathVariable("id") String id){
+        employeeService.deleteEmployee(id);
+    }
 }
