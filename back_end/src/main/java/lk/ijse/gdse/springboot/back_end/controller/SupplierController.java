9312de9898bf869a2b7153d39417c09d072278d7
MSI <chathurabhashitha01@gmail.com>
2024-05-03 11:41:50 +0530

    suppliercontroller creation

--- back_end/src/main/java/lk/ijse/gdse/springboot/back_end/controller/SupplierController.java
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/controller/SupplierController.java
@@ -1,4 +1,44 @@
 package lk.ijse.gdse.springboot.back_end.controller;
 
+import lk.ijse.gdse.springboot.back_end.dto.SupplierDTO;
+import lk.ijse.gdse.springboot.back_end.service.SupplierService;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/supplier")
 public class SupplierController {
+    private SupplierService supplierService;
+
+    public SupplierController(SupplierService supplierService) {
+        this.supplierService = supplierService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    private List<SupplierDTO> getAllSupplier(){
+        return supplierService.getAllSupplier();
+    }
+
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public SupplierDTO getSupplierDetails(@PathVariable("id") String id){
+        return supplierService.getSupplierDetails(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void saveSupplier(@RequestBody SupplierDTO supplierDTO){
+        supplierService.saveSupplier(supplierDTO);
+    }
+
+    @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateSupplier(@RequestBody SupplierDTO supplierDTO){
+        supplierService.updateSupplier(supplierDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteSupplier(@PathVariable String id){
+        supplierService.deleteSupplier(id);
+    }
+
 }
