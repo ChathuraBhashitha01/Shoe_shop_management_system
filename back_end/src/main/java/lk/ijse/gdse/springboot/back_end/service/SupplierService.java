efa0de29c4164cdac60b81416a44b9268a3abb04
MSI <chathurabhashitha01@gmail.com>
2024-05-03 11:26:40 +0530

    supplier serviceimpl creation

--- back_end/src/main/java/lk/ijse/gdse/springboot/back_end/service/SupplierService.java
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/service/SupplierService.java
@@ -8,7 +8,7 @@ public interface SupplierService {
 
     List<SupplierDTO> getAllSupplier();
     SupplierDTO getSupplierDetails(String id);
-    void saveSupplier(SupplierDTO customerDTO);
-    void updateSupplier(SupplierDTO customerDTO);
+    void saveSupplier(SupplierDTO supplierDTO);
+    void updateSupplier(SupplierDTO supplierDTO);
     void deleteSupplier(String id);
 }
--- back_end/src/main/java/lk/ijse/gdse/springboot/back_end/service/impl/SupplierServiceImpl.java
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/service/impl/SupplierServiceImpl.java
@@ -1,7 +1,10 @@
 package lk.ijse.gdse.springboot.back_end.service.impl;
 
 import lk.ijse.gdse.springboot.back_end.dto.SupplierDTO;
+import lk.ijse.gdse.springboot.back_end.entity.Supplier;
+import lk.ijse.gdse.springboot.back_end.repository.SupplierRepo;
 import lk.ijse.gdse.springboot.back_end.service.SupplierService;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,28 +12,36 @@ import java.util.List;
 @Service
 public class SupplierServiceImpl implements SupplierService {
 
+    private ModelMapper modelMapper;
+    private SupplierRepo supplierRepo;
+
+    public SupplierServiceImpl(ModelMapper modelMapper, SupplierRepo supplierRepo) {
+        this.modelMapper = modelMapper;
+        this.supplierRepo = supplierRepo;
+    }
+
     @Override
     public List<SupplierDTO> getAllSupplier() {
-        return null;
+        return supplierRepo.findAll().stream().map(supplier -> modelMapper.map(supplier,SupplierDTO.class)).toList();
     }
 
     @Override
     public SupplierDTO getSupplierDetails(String id) {
-        return null;
+        return modelMapper.map(supplierRepo.findById(id).get(),SupplierDTO.class);
     }
 
     @Override
-    public void saveSupplier(SupplierDTO customerDTO) {
-
+    public void saveSupplier(SupplierDTO supplierDTO) {
+        supplierRepo.save(modelMapper.map(supplierDTO, Supplier.class));
     }
 
     @Override
-    public void updateSupplier(SupplierDTO customerDTO) {
-
+    public void updateSupplier(SupplierDTO supplierDTO) {
+        supplierRepo.save(modelMapper.map(supplierDTO, Supplier.class));
     }
 
     @Override
     public void deleteSupplier(String id) {
-
+        supplierRepo.deleteById(id);
     }
 }
