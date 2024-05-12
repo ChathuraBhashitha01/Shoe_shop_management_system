4ae84c52a1366a38e958f399b97305f6056df9df
MSI <chathurabhashitha01@gmail.com>
2024-05-12 11:08:35 +0530

    sale service impl is creation

--- back_end/src/main/java/lk/ijse/gdse/springboot/back_end/service/impl/SaleServiceImpl.java
+++ back_end/src/main/java/lk/ijse/gdse/springboot/back_end/service/impl/SaleServiceImpl.java
@@ -1,4 +1,23 @@
 package lk.ijse.gdse.springboot.back_end.service.impl;
 
-public class SaleServiceImpl {
+import lk.ijse.gdse.springboot.back_end.dto.SaleDTO;
+import lk.ijse.gdse.springboot.back_end.entity.Sale;
+import lk.ijse.gdse.springboot.back_end.repository.SaleRepo;
+import lk.ijse.gdse.springboot.back_end.service.SaleService;
+import org.modelmapper.ModelMapper;
+
+public class SaleServiceImpl implements SaleService {
+
+    private ModelMapper modelMapper;
+    private SaleRepo saleRepo;
+
+    public SaleServiceImpl(ModelMapper modelMapper, SaleRepo saleRepo) {
+        this.modelMapper = modelMapper;
+        this.saleRepo = saleRepo;
+    }
+
+    @Override
+    public void saveDetails(SaleDTO saleDTO) {
+        saleRepo.save(modelMapper.map(saleDTO, Sale.class));
+    }
 }
