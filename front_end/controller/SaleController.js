88b9be50b82f0e749abc163580e946b45b7f4248
MSI <chathurabhashitha01@gmail.com>
2024-05-15 01:36:14 +0530

    sale form controller creation

--- front_end/controller/SaleController.js
+++ front_end/controller/SaleController.js
@@ -9,6 +9,7 @@ function getAllInventoriesForSale(){
         success: function (resp) {
             for (const inventory of resp) {
                 let divElement = document.createElement("div");
+                divElement.setAttribute("class","itemDetailsDiv")
                 $("#itemDetails").append(divElement);
 
                 let imageElement = `<img alt="image" src="data:image/png;base64,${inventory.itemPicture}" style="width: 200px; height: 150px; padding: 0;">`
@@ -22,6 +23,31 @@ function getAllInventoriesForSale(){
 
                 let buttonElement=`<button>Buy</button>`;
                 $("body>main>section:nth-child(5)>section:first-child>div").append(buttonElement);
+
+                let status=`<h4 class="hoverAction">${inventory.status}</h4>`
+                $("body>main>section:nth-child(5)>section:first-child>div").append(status);
+
+                let size=`<h4 class="hoverAction">${inventory.status}</h4>`
+                $("body>main>section:nth-child(5)>section:first-child>div").append(size);
+
+                let stock=`<h4 class="hoverAction">In stock</h4>`
+                $("body>main>section:nth-child(5)>section:first-child>div").append(stock);
+
+
+                // $("body>main>section:nth-child(5)>section:first-child>div").addEventListener("mouseenter",function (){
+                //     $("body>main>section:nth-child(5)>section:first-child>div>h4:nth-child(5)").style('display', 'inline-block');
+                //     $("body>main>section:nth-child(5)>section:first-child>div>h4:nth-child(6)").style('display', 'inline-block');
+                //     $("body>main>section:nth-child(5)>section:first-child>div>h4:nth-child(7)").style('display', 'inline-block');
+                // })
+                //
+                // $("body>main>section:nth-child(5)>section:first-child>div").addEventListener("mouseleave",function (){
+                //     $("body>main>section:nth-child(5)>section:first-child>div>h4:nth-child(5)").style('display', 'none');
+                //     $("body>main>section:nth-child(5)>section:first-child>div>h4:nth-child(6)").style('display', 'none');
+                //     $("body>main>section:nth-child(5)>section:first-child>div>h4:nth-child(7)").style('display', 'none');
+                // })
+
+
+
             }
         }
     });
